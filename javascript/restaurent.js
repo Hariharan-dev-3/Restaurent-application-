a17c389bc461b517a7d861d87dcfd17d166052e9
Hariharan3233 <haritemp33@mail.com>
2025-06-19 10:30:17 +0530

    new script

--- javascript/restaurent.js
+++ javascript/restaurent.js
@@ -3,6 +3,7 @@ const closeBtn=document.querySelector('.Close');
 const imageContainer=document.querySelector('.imageContainer');
 const menuitem=document.querySelector('.item');
 const listBox=document.querySelector('.box');
+const displayMenu=document.getElementById('items');
 
 
 document.addEventListener('DOMContentLoaded',()=>{
@@ -28,25 +29,44 @@ document.addEventListener('DOMContentLoaded',()=>{
         imageContainer.appendChild(image);
     })
 
+    const menuItemsList=[
+        {
+            foodImage:"biriyani.png",
+            foodName:"Biriyani",
+            FoodRate:"200.00",
+            type:"non-veg"
+        },
+        {
+            foodImage:"panneeer.png",
+            foodName:"Panner",
+            FoodRate:"150.00",
+            type:"veg"
+        }
+    ]
+
+    menuItemsList.forEach(items=>{
+        const itemdiv=document.createElement('div').setAttribute('class','item');
+        const MenuImage=document.createElement('img').setAttribute('class','logo');
+        MenuImage.src=`../Image/${items.foodImage}`;
+        const textdiv=document.createElement('div').setAttribute('class','texts');
+        const Fname=document.createElement('p').setAttribute('class','foodName');
+        Fname.textContent=items.foodName;
+        const Frate=document.createElement('p').setAttribute('class','foodRate');
+        Frate.textContent=items.FoodRate;
+
+        textdiv.appendChild(Fname);
+        textdiv.appendChild(Frate);
+        itemdiv.appendChild(MenuImage);
+        itemdiv.appendChild(textdiv);
+        displayMenu.appendChild(itemdiv);
 
 
-        // window.addEventListener('scroll',()=>{
-        //     let navheight=navbar.offsetHeight;
-        //     let scrollheight=window.scrollY;
-        //     let displayStatus = window.getComputedStyle(menu).display;
-            
+        })
 
-        //     console.log('navbar height',navheight);
-        //     console.log('scroll height',scrollheight);
-        //      console.log('status',displayStatus);
 
-        //     if(scrollheight>navheight){
-        //             menu.style.display="block";
-        //     }
-        //     else{
-        //         menu.style.display="none";
-        //     }
-        // })
+
+
+
 
 })
 
