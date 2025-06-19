649acff97b2167ef861f565edccad9d9abcb4484
Hariharan3233 <haritemp33@mail.com>
2025-06-19 12:45:06 +0530

    menu page

--- javascript/restaurent.js
+++ javascript/restaurent.js
@@ -4,6 +4,8 @@ const imageContainer=document.querySelector('.imageContainer');
 const menuitem=document.querySelector('.item');
 const listBox=document.querySelector('.box');
 const displayMenu=document.getElementById('items');
+const selectbutton=document.getElementById('selectButton');
+
 
 
 document.addEventListener('DOMContentLoaded',()=>{
@@ -41,27 +43,54 @@ document.addEventListener('DOMContentLoaded',()=>{
             foodName:"Panner",
             FoodRate:"150.00",
             type:"veg"
+        },
+        {
+            foodImage:"veg.png",
+            foodName:"Meals",
+            FoodRate:"180.00",
+            type:"veg"
+        },
+        {
+            foodImage:"chips.png",
+            foodName:"Fried",
+            FoodRate:"190.00",
+            type:"non-veg"
         }
     ]
 
+    
     menuItemsList.forEach(items=>{
-        const itemdiv=document.createElement('div').setAttribute('class','item');
-        const MenuImage=document.createElement('img').setAttribute('class','logo');
-        MenuImage.src=`../Image/${items.foodImage}`;
-        const textdiv=document.createElement('div').setAttribute('class','texts');
-        const Fname=document.createElement('p').setAttribute('class','foodName');
+        const itemdiv=document.createElement('div');
+        itemdiv.setAttribute('class','item');
+        const MenuImage=document.createElement('img');
+        MenuImage.setAttribute('class','logo');
+        MenuImage.src=`../Images/${items.foodImage}`;
+        const textdiv=document.createElement('div');
+        textdiv.setAttribute('class','texts');
+        const Fname=document.createElement('p');
+        Fname.setAttribute('class','foodName');
         Fname.textContent=items.foodName;
-        const Frate=document.createElement('p').setAttribute('class','foodRate');
+        const Frate=document.createElement('p');
+        Frate.setAttribute('class','foodRate');
         Frate.textContent=items.FoodRate;
+        const Btn=document.createElement('button');
+        Btn.setAttribute('class','orderBtn');
+        Btn.textContent="Order Now";
 
         textdiv.appendChild(Fname);
         textdiv.appendChild(Frate);
         itemdiv.appendChild(MenuImage);
         itemdiv.appendChild(textdiv);
+        itemdiv.appendChild(Btn);
         displayMenu.appendChild(itemdiv);
 
 
         })
+
+
+selectbutton.addEventListener('change',()=>{
+
+})
 
 
 
